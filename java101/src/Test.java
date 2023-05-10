5aff3fa3032d5991ae26d66069e97d47a5de89a5
quas1modo <75099936+quas1modo@users.noreply.github.com>
2023-05-10 13:55:36 +0300

    Add files via upload

---
+++ java101/src/Test.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = input.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >=0  ; i--) {
+            for (int j = (n - i); j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >=1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
